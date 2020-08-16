85cd08a303922a72aae8dd316e0f4a0c30a6156e
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2020-08-16 15:37:23 +0200

    feat: added Segment component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Colors } from "./Colors";
+
+interface SegmentProps {
+  index: number;
+  color: string;
+}
+
+function Segment({ index, color }: SegmentProps) {
+  const colors = [
+    Colors.Red,
+    Colors.Green,
+    Colors.Blue,
+    Colors.Yellow,
+    Colors.Pink,
+    Colors.Lime,
+    Colors.Orange,
+  ];
+
+  const segmentStyle = {
+    height: "1rem",
+    width: `${index + 1}rem`,
+    background: colors[index],
+  };
+
+  return <div style={segmentStyle} />;
+}
 
 function App() {
   return (
     <div className="App">
-      Lol
+      <Segment index={0} color={"red"} />
     </div>
   );
-}g
+}
 
 export default App;
