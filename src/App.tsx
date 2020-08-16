18a72ff9900f2238963f0357d0d72f67f04a5105
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2020-08-16 18:36:34 +0200

    feat: added Stack and Segment component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import { Colors } from "./Colors";
 
 interface SegmentProps {
   index: number;
-  color: string;
 }
 
-function Segment({ index, color }: SegmentProps) {
+function Segment({ index }: SegmentProps) {
   const colors = [
     Colors.Red,
     Colors.Green,
@@ -16,21 +15,45 @@ function Segment({ index, color }: SegmentProps) {
     Colors.Pink,
     Colors.Lime,
     Colors.Orange,
+    Colors.Purple,
   ];
 
   const segmentStyle = {
     height: "1rem",
     width: `${index + 1}rem`,
     background: colors[index],
+    borderRadius: "4px",
   };
 
   return <div style={segmentStyle} />;
 }
 
+interface StackProps {
+  segments: Array<number>;
+}
+
+function Stack({ segments }: StackProps) {
+  const stackStyle = {
+    width: `${Math.max(...segments)}rem`,
+  };
+
+  return (
+    <div className="stack" style={stackStyle}>
+      {segments.map((segmentIndex) => (
+        <Segment index={segmentIndex} key={segmentIndex} />
+      ))}
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <Segment index={0} color={"red"} />
+      <div className="wrapper">
+        <Stack segments={[0, 1, 5]} />
+        <Stack segments={[2, 3, 6]} />
+        <Stack segments={[4, 7]} />
+      </div>
     </div>
   );
 }
