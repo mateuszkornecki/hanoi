980ca7854aa712adf34738aa0e02bdb02fabc81f
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2020-08-19 21:05:22 +0200

    refactor: changed property name

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,7 +7,7 @@ interface Stacks {
   third: Array<number>;
 }
 
-interface StacksToMove {
+interface StacksToChange {
   from: keyof Stacks | null;
   to: keyof Stacks | null;
 }
@@ -18,47 +18,51 @@ function Board() {
     second: [],
     third: [0, 1, 2, 3, 5],
   };
-  const initialState: StacksToMove = { from: null, to: null };
+  const initialState: StacksToChange = { from: null, to: null };
   const [stacks, setStacks] = useState(initialStacksConfig);
-  const [state, setState] = useState(initialState);
+  const [stacksToChange, setStacksToChange] = useState(initialState);
+
+  const targetStack = stacks[stacksToChange.to as keyof Stacks];
 
   useEffect(() => {
-    setState(initialState);
+    setStacksToChange(initialState);
     moveSegments();
-  }, [stacks[state.to as keyof Stacks]]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [targetStack]);
 
   const setStacksToEdit = (id: string) => {
-    if (!state.from && (stacks as any)[id].length) {
-      setState((prevState) => ({ ...prevState, from: id } as any));
-    } else if (state.from && state.from !== id) {
-      setState((prevState) => ({ ...prevState, to: id } as any));
+    if (!stacksToChange.from && (stacks as any)[id].length) {
+      setStacksToChange((prevState) => ({ ...prevState, from: id } as any));
+    } else if (stacksToChange.from && stacksToChange.from !== id) {
+      setStacksToChange((prevState) => ({ ...prevState, to: id } as any));
     } else {
-      setState(initialState);
+      setStacksToChange(initialState);
     }
   };
 
   const moveSegments = (): void => {
     const isLegalMove =
-      state.from &&
-      state.to &&
-      (!stacks[state.to].length || stacks[state.from][0] < stacks[state.to][0]);
+      stacksToChange.from &&
+      stacksToChange.to &&
+      (!stacks[stacksToChange.to].length ||
+        stacks[stacksToChange.from][0] < stacks[stacksToChange.to][0]);
 
     if (isLegalMove) {
       setStacks((prevState) => {
-        const start = prevState[state.from as keyof Stacks];
-        const end = prevState[state.to as keyof Stacks];
+        const start = prevState[stacksToChange.from as keyof Stacks];
+        const end = prevState[stacksToChange.to as keyof Stacks];
         const newStart = [...start];
         newStart.splice(0, 1);
         const newEnd = prevState.second ? [start[0], ...end] : [start[0]];
 
         const newStacks = { ...prevState };
-        newStacks[state.from as keyof Stacks] = newStart;
-        newStacks[state.to as keyof Stacks] = newEnd;
+        newStacks[stacksToChange.from as keyof Stacks] = newStart;
+        newStacks[stacksToChange.to as keyof Stacks] = newEnd;
         return newStacks;
       });
     } else {
       console.log("invalid move");
-      setState(initialState);
+      setStacksToChange(initialState);
     }
   };
 
