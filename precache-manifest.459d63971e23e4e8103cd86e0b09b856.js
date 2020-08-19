c5299d6f261c094b2228a7aed41773d8480da514
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2020-08-19 21:08:55 +0200

    Updates

---
+++ precache-manifest.459d63971e23e4e8103cd86e0b09b856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c956586b2d23a88adc7c902087ef08",
+    "url": "/hanoi/index.html"
+  },
+  {
+    "revision": "7c3019624a8bc1d3107b",
+    "url": "/hanoi/static/css/main.cb7c3e55.chunk.css"
+  },
+  {
+    "revision": "b886b49a830aba93d674",
+    "url": "/hanoi/static/js/2.f0f81535.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hanoi/static/js/2.f0f81535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3019624a8bc1d3107b",
+    "url": "/hanoi/static/js/main.7af7dc44.chunk.js"
+  },
+  {
+    "revision": "f63cbc54d6319295c491",
+    "url": "/hanoi/static/js/runtime-main.f33cb55d.js"
+  }
+]);
