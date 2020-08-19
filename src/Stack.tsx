e158f236064112b47f0915515605f4a31c6415b5
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2020-08-19 07:56:39 +0200

    feat: added stackIndex props

--- src/Stack.tsx
+++ src/Stack.tsx
@@ -3,16 +3,19 @@ import "./App.scss";
 import { Segment } from "./Segment";
 
 export interface StackProps {
+  stackIndex: string;
   segments: Array<number>;
 }
 
-export function Stack({ segments }: StackProps) {
+export function Stack({ segments, stackIndex }: StackProps) {
   const stackStyle = {
-    width: `${Math.max(...segments)}rem`,
+    width: "10rem",
+    height: "10rem",
+    background: "lightgray",
   };
 
   return (
-    <div className="stack" style={stackStyle}>
+    <div className="stack" id={stackIndex} style={stackStyle}>
       {segments.map((segmentIndex) => (
         <Segment index={segmentIndex} key={segmentIndex} />
       ))}
